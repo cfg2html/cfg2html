7efe7a0f7f387663c7bc6492ec9e390eef729290
Gratien D'haese <gratien.dhaese@hp.com>
2013-04-26 17:47:29 +0200

    Made some final customization of HP-UX SD depot creation process The crontab entry will start on a random hour:minute and day

--- hpux/packaging/postinstall.sh
+++ hpux/packaging/postinstall.sh
@@ -9,7 +9,7 @@ echo "       * current active crontab file saved as $cronfile"
 # remove old redundant entries
 egrep -i -v 'cfg2html' $cronfile  > $cronfile.new
 # add new entry (built in a sleep up to 600 seconds to avoid traffic jams)
-echo "6 3 * * 0  /usr/bin/sleep \$((RANDOM % 600)) ; /usr/sbin/cfg2html -2%Y%m%d > /dev/null 2>&1" >> $cronfile.new
+echo "$((RANDOM % 55)) $((RANDOM % 5)) * * $((RANDOM % 5))  /usr/bin/sleep \$((RANDOM % 600)) ; /usr/sbin/cfg2html -2%Y%m%d > /dev/null 2>&1" >> $cronfile.new
 
 # activate the new crontab file
 crontab $cronfile.new
