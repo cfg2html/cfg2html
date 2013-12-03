c572e706bb70e9429c436ace777b45c723ae227c
Gratien D'haese <gratien.dhaese@hp.com>
2013-12-03 16:01:05 +0100

    the crontab entry of cfg2html is now commented out by default; added an extra line of comment to explain the options; close #30

--- hpux/packaging/postinstall.sh
+++ hpux/packaging/postinstall.sh
@@ -11,13 +11,14 @@ echo "       * current active crontab file saved as $cronfile"
 # remove old redundant entries
 egrep -i -v 'cfg2html' $cronfile  > $cronfile.new
 # add new entry (built in a sleep up to 600 seconds to avoid traffic jams)
-echo "$((RANDOM % 55)) $((RANDOM % 5)) * * $((RANDOM % 5))  /usr/bin/sleep \$((RANDOM \% 600)) ; /usr/sbin/cfg2html -2\%Y\%m\%d > /dev/null 2>&1" >> $cronfile.new
+echo "# Uncomment following line to schedule a weekly cfg2html run ( added on $(date '+%d %b %Y') )" >> $cronfile.new
+echo "# $((RANDOM % 55)) $((RANDOM % 5)) * * $((RANDOM % 5))  /usr/bin/sleep \$((RANDOM \% 600)) ; /usr/sbin/cfg2html -2\%Y\%m\%d > /dev/null 2>&1" >> $cronfile.new
 
 # activate the new crontab file
 crontab $cronfile.new
 
 # show the added line
-echo "       * Added line to crontab:"
+echo "       * Added lines to crontab:"
 echo "         $(crontab -l | grep cfg2html)"
 
 # cleanup
