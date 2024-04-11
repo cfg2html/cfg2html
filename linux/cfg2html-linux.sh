68ef1b17316c32152ddfc1c6c4bb56fb9ab99d18
Ed Drouillard <edrulrd@hotmail.com>
2024-04-11 11:39:21 -0400

    chg: dev: confirm "lsblk -p" option is available

--- linux/cfg2html-linux.sh
+++ linux/cfg2html-linux.sh
@@ -1421,7 +1421,7 @@ inc_heading_level
     fi
 
     if [ -x "$(which lsblk 2>/dev/null)" ] ; then # use lsblk to get our disks, which might get us a few extras # modified on 20240322 by edrulrd
-      Diskdevs=$(lsblk -p | grep "^/" | grep disk | "${AWKCMD}" '{print $1}') # get the block devices, but only those marked as disk eg. /dev/sda, not lv's etc.
+      Diskdevs=$(lsblk -p 2>/dev/null | grep "^/" | grep disk | "${AWKCMD}" '{print $1}') # get the block devices, but only those marked as disk eg. /dev/sda, not lv's etc. # del errors if -p not availabe # modified on 20240411 by edrulrd
     else
       if [ -x "$(which "${SMARTCTL}" 2>/dev/null)" ] ; then # if lsblk not available, use smartctl instead to get only disk devices # modified on 20240322 by edrulrd
         Diskdevs=$("${SMARTCTL}" --scan | "${AWKCMD}" '{print $1}') # only use drives smartctl knows about # modified on 20240322 by edrulrd
